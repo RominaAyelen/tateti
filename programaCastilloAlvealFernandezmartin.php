33788e1f45f36d1295f8d244d24a2f652fde9edb
RominaAyelen <romina.castillo@est.fi.uncoma.edu.ar>
2021-11-28 18:26:11 -0300

    creando menu

--- programaCastilloAlvealFernandezmartin.php
+++ programaCastilloAlvealFernandezmartin.php
@@ -48,27 +48,46 @@ $juego = jugar();
 //imprimirResultado($juego);
 
 
-echo"Ingrese un numero:";
-/*
+echo"1) Jugar a tateti \n";
+echo"2) Mostrar un juego \n";
+echo"3) Mostrar el primer juego ganado \n";
+echo"4) Mostrar porcentaje de Juegos ganados \n";
+echo"5) Mostrar resumen de un Jugador\n";
+echo"6) Mostrar listado de juegos ordenados por jugador O \n";
+echo"7) Salir \n";
+
 do {
+    echo"INGRESE UN NUMERO ";
     $opcion =trim(fgets(STDIN));
-
-    
     switch ($opcion) {
-        case 1: 
+        case ($opcion == "1"): 
             //completar qué secuencia de pasos ejecutar si el usuario elige la opción 1
-
+            echo"JUGANDO AL TATETI \n";
+            $juego = jugar();
             break;
-        case 2: 
+        case ($opcion == "2"):    
             //completar qué secuencia de pasos ejecutar si el usuario elige la opción 2
-
+            echo"funciona  2 \n";
             break;
-        case 3: 
+        case ($opcion == "3"): 
             //completar qué secuencia de pasos ejecutar si el usuario elige la opción 3
-
+            echo"funciona  3 \n";
             break;
-        
-        case
+        case ($opcion == "4"): 
+                //completar qué secuencia de pasos ejecutar si el usuario elige la opción 1
+                echo"funciona  4 \n";
+                break;
+        case ($opcion == "5"):    
+                //completar qué secuencia de pasos ejecutar si el usuario elige la opción 2
+                echo"funciona  5 \n";
+                break;
+        case ($opcion == "6"): 
+                //completar qué secuencia de pasos ejecutar si el usuario elige la opción 3
+                echo"funciona  6 \n";
+                break;
+        case ($opcion == "7"): 
+                //completar qué secuencia de pasos ejecutar si el usuario elige la opción 3
+                echo"funciona  7 \n";
+                break;
     }
-} while ($opcion != X);
-*/
+} while ($opcion <> 7);
